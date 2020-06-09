7ab84695d2cbfbd132e9589c602c7aef1a82c9ba
kampon nontapap <kampon.nontapap@ktb.co.th>
2020-06-09 15:46:07 +0700

    delete unuse code

--- src/main/java/com/dsl/Application.java
+++ src/main/java/com/dsl/Application.java
@@ -2,8 +2,10 @@ package com.dsl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+//@SpringBootApplication //when use datasource in application.yml
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class}) //when don't use datasource
 public class Application {
 
 	public static void main(String[] args) {
